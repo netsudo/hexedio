d7c6a72aff82b716576eb81e28009191c462f944
netsudo <netsudo@users.noreply.github.com>
2018-05-30 19:29:03 -0700

    Added auth pipelines for router and error handler helpers

---
+++ lib/hexedio/auth/pipeline.ex
@@ -0,0 +1,16 @@
+defmodule Hexedio.Auth.Pipeline do
+  use Guardian.Plug.Pipeline, 
+    otp_app: :auth_ex,
+    error_handler: Hexedio.Auth.ErrorHandler,
+    module: Hexedio.Auth.Guardian
+
+  #If there's a session token, validate it
+  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+
+  #If there's an auth header, validate it
+  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+
+  #Load the user if either of the verifications worked
+  plug Guardian.Plug.LoadResource, allow_blank: true
+
+end
