209aa06a1478a92fa5931c9fda526cb78945d770
distant <netsudo>
2018-06-22 19:38:38 -0700

    Changed up how put_assoc is working, moved the logic out of the posts handler and into the changset

--- lib/hexedio/posts/category.ex
+++ lib/hexedio/posts/category.ex
@@ -6,7 +6,7 @@ defmodule Hexedio.Posts.Category do
   schema "categories" do
     field :name, :string
 
-    many_to_many :posts, Hexedio.Posts.Post, join_through: "posts_categories"
+    many_to_many :posts, Hexedio.Posts.Post, [join_through: "posts_categories", on_replace: :delete]
 
     timestamps()
   end
@@ -16,5 +16,6 @@ defmodule Hexedio.Posts.Category do
     category
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> unique_constraint(:name)
   end
 end
--- lib/hexedio/posts/post.ex
+++ lib/hexedio/posts/post.ex
@@ -5,6 +5,9 @@ end
 defmodule Hexedio.Posts.Post do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+  alias Hexedio.Repo
+  alias Hexedio.Posts.Category
   alias Hexedio.Posts.Post.TitleSlug
 
 
@@ -15,16 +18,20 @@ defmodule Hexedio.Posts.Post do
     field :title, :string
     field :slug, TitleSlug.Type
     
-    many_to_many :categories, Hexedio.Posts.Category, join_through: "posts_categories"
+    many_to_many :categories, Hexedio.Posts.Category, [join_through: "posts_categories", on_replace: :delete]
 
     timestamps()
   end
 
   @doc false
-  def changeset(post, attrs \\ %{}) do
+  def changeset(post, attrs \\ %{"categories" => []}) do
+    IO.inspect attrs
+    cat = Repo.all(from c in Category, where: c.name in ^attrs["categories"])
+
     post
+    |> Repo.preload(:categories)
     |> cast(attrs, [:title, :content, :date_published, :published])
-    |> cast_assoc(:categories)
+    |> put_assoc(:categories, cat)
     |> validate_required([:title, :content, :date_published, :published, :categories])
     #Using ecto-autoslug-field to generate the slug
     |> unique_constraint(:title)
@@ -32,4 +39,3 @@ defmodule Hexedio.Posts.Post do
     |> TitleSlug.unique_constraint
   end
 end
-
