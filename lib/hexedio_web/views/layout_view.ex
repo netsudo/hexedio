a20cf3daadd23229b4a57a4ac029b844c8e46c95
netsudo <netsudo@users.noreply.github.com>
2018-06-28 21:13:26 -0700

    Categories are being displayed in the base layout with hyperlinks to the category pages

--- lib/hexedio_web/views/layout_view.ex
+++ lib/hexedio_web/views/layout_view.ex
@@ -1,9 +1,14 @@
 defmodule HexedioWeb.LayoutView do
   use HexedioWeb, :view
   alias Hexedio.Auth.Guardian
+  alias Hexedio.Posts
 
   def maybe_user(conn) do
     Guardian.Plug.current_resource(conn)
+  end
+
+  def categories() do
+    Posts.list_categories
   end
 
 end
