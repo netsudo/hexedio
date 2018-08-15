1523b6ebcb73058986f0ee7a0fe4d9eba6fabe03
netsudo <netsudo@users.noreply.github.com>
2018-08-15 15:47:00 -0700

    Added virtual schema for contact form

---
+++ lib/hexedio/contact/contact.ex
@@ -0,0 +1,20 @@
+defmodule ContactForm do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "" do
+    field :email, :string, virtual: true
+    field :subject, :string, virtual: true
+    field :body, :string, virtual: true
+  end
+
+  def changeset(contact, attrs \\ %{}) do
+    model
+    |> cast(attrs, [:email, :subject, :body])
+    |> validate_required([:name, :subject, :body])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:email, max: 50)
+    |> validate_length(:subject, min: 2)
+    |> validate_length(:subject, max: 50)
+    |> validate_length(:body, min: 20)
+    |> validate_length(:subject, max: 3000)
