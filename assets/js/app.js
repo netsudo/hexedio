273b4e6b980022123f23f6c16fdacfe1a184f345
netsudo <netsudo@users.noreply.github.com>
2018-08-30 21:05:31 -0700

    Added highlightjs as a npm package instead. Have it highlighting code currently

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,8 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import hljs from "highlight.js"
+hljs.initHighlightingOnLoad();
 
 // Import local files
 //
