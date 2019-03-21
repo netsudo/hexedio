4e4c3ad4a10b82251942057fac358fa6d91d5c49
netsudo <netsudo@users.noreply.github.com>
2019-03-21 00:31:35 -0700

    Auth attempt genserv to keep track of user login attempts

--- lib/hexedio/auth/auth_attempts.ex
+++ lib/hexedio/auth/auth_attempts.ex
@@ -1,33 +1,61 @@
 defmodule Hexedio.LoginAttempt do
-  use Agent
+  use GenServer
 
   @doc """
   Starts the LoginAttempt agent to keep track of login attempts
   """
   def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+    GenServer.start_link(__MODULE__, :empty, name: __MODULE__)
   end
+
+  def init(:empty), do: {:ok, %{}}
 
   @doc """
   Get the number of attempts by a username
   """
   def get(username) do
-    Agent.get(__MODULE__, &Map.get(&1, username))
+    GenServer.call(__MODULE__, {:lookup, username})
   end
 
   @doc """
   Adds or increments user when a login attempt is made
   """
   def make(username) do
-    get(username) |> update(username)
+    GenServer.call(__MODULE__, {:update, username})
   end
 
-  defp update(nil, username) do 
-    Agent.update(__MODULE__, &Map.put(&1, username, 1))
+  @doc """
+  Removes the user from the agent so it can be restarted cleanly
+  """
+  def start_timer(username) do
+    receive do
+    after
+      6_000 -> GenServer.call(__MODULE__, {:stop, "Timer ended.", 0})
+    end
   end
 
-  defp update(attempts, username) when is_integer(attempts) do 
-    Agent.update(__MODULE__, &Map.put(&1, username, attempts + 1))
+  def handle_call({:lookup, username}, _from, index_map) do
+    {:reply, get_user_attempts(username, index_map), index_map}
+  end
+
+  def handle_call({:update, username}, _from, index_map) do 
+    tracked_user? = Map.has_key?(index_map, username)
+    {:reply, :ok, increment_or_create(username, index_map, tracked_user?)}
+  end
+
+  defp increment_or_create(username, index_map, _tracked_user = true) do
+    Map.update!(index_map, username, &(&1 + 1))
+  end
+
+  defp increment_or_create(username, index_map, _tracked_user = false) do
+    Map.put(index_map, username, 1)
+  end
+
+  defp get_user_attempts(username, index_map) do
+    case Map.get(index_map, username) do
+      nil          -> {:error, :not_found}
+      _ = attempts -> {:ok, attempts}
+    end
   end
 
 end
