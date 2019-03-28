1e0f262b2ffebcc6875fb278c2bb02b5a14133ca
netsudo <netsudo@users.noreply.github.com>
2019-03-28 02:37:18 -0700

    Moved back to agent, just going to compare dates. More efficient and reliable

--- lib/hexedio/auth/login_attempts/auth_attempts.ex
+++ lib/hexedio/auth/login_attempts/auth_attempts.ex
@@ -1,62 +1,53 @@
 defmodule Hexedio.LoginAttempt do
-  use GenServer
+  use Agent
 
   @doc """
   Starts the LoginAttempt agent to keep track of login attempts
   """
   def start_link do
-    GenServer.start_link(__MODULE__, :empty, name: __MODULE__)
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
-
-  def init(:empty), do: {:ok, %{}}
 
   @doc """
   Get the number of attempts by a username
   """
   def get(username) do
-    GenServer.call(__MODULE__, {:lookup, username})
+    Agent.get(__MODULE__, &Map.get(&1, username))
   end
 
   @doc """
   Adds or increments user when a login attempt is made
   """
   def make(username) do
-    GenServer.call(__MODULE__, {:update, username})
+    get(username) |> update(username)
   end
 
-  @doc """
-  Removes the user from the agent so it can be restarted cleanly
-  """
-  def reset(username) do
-    GenServer.cast(__MODULE__, {:reset, username})
+  defp update(nil, username) do 
+    Agent.update(__MODULE__, 
+                 &Map.put(&1, username, {1, expiry_date()}))
   end
 
-  def handle_call({:lookup, username}, _from, index_map) do
-    {:reply, get_user_attempts(username, index_map), index_map}
+  defp update(attempts, username) when is_integer(attempts) do 
+    Agent.update(__MODULE__, 
+                 &Map.put(&1, username, attempts + 1))
   end
 
-  def handle_call({:update, username}, _from, index_map) do 
-    tracked_user? = Map.has_key?(index_map, username)
-    {:reply, :ok, increment_or_create(username, index_map, tracked_user?)}
-  end
-
-  def handle_cast({:reset, username}, index_map) do
-    {:noreply, Map.delete(index_map, username)}
-  end
-
-  defp increment_or_create(username, index_map, _tracked_user = true) do
-    Map.update!(index_map, username, &(&1 + 1))
-  end
-
-  defp increment_or_create(username, index_map, _tracked_user = false) do
-    Map.put(index_map, username, 1)
-  end
-
-  defp get_user_attempts(username, index_map) do
-    case Map.get(index_map, username) do
-      nil          -> {:error, :not_found}
-      _ = attempts -> {:ok, attempts}
+  defp update(%DateTime{} = expiry, username) do
+    cond do
+      expiry <= current_time() -> IO.puts "Reset attempts"
+      expiry >  current_time() -> IO.puts "Add new attempt"
     end
   end
 
+  defp current_time do
+    {_, datetime} = DateTime.now("Etc/UTC")
+    datetime
+  end
+
+  defp expiry_date do
+    DateTime.now("Etc/UTC")
+    |> elem(1)
+    # Add 15 minutes to the current date
+    |> DateTime.add(15*60, :second)
+  end
 end
