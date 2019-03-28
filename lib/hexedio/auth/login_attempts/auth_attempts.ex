73ef608d3b80514baf6edc578299ca160e20962f
netsudo <netsudo@users.noreply.github.com>
2019-03-28 14:43:04 -0700

    Finished up the logic for setting and updating the expiry dates and attempt counter

--- lib/hexedio/auth/login_attempts/auth_attempts.ex
+++ lib/hexedio/auth/login_attempts/auth_attempts.ex
@@ -27,15 +27,15 @@ defmodule Hexedio.LoginAttempt do
                  &Map.put(&1, username, {1, expiry_date()}))
   end
 
-  defp update(attempts, username) when is_integer(attempts) do 
+  defp update(attempts, username) do 
     Agent.update(__MODULE__, 
-                 &Map.put(&1, username, attempts + 1))
+                 &Map.put(&1, username, set_attempt(attempts)))
   end
 
-  defp update(%DateTime{} = expiry, username) do
-    cond do
-      expiry <= current_time() -> IO.puts "Reset attempts"
-      expiry >  current_time() -> IO.puts "Add new attempt"
+  defp set_attempt({attempts, %DateTime{} = expiry}) do
+    case DateTime.compare(expiry, current_time()) do
+      :lt -> {1, expiry_date()}
+      :gt -> {attempts + 1, expiry_date()}
     end
   end
 
@@ -48,6 +48,6 @@ defmodule Hexedio.LoginAttempt do
     DateTime.now("Etc/UTC")
     |> elem(1)
     # Add 15 minutes to the current date
-    |> DateTime.add(15*60, :second)
+    |> DateTime.add(30, :second)
   end
 end
